70947b7c8d7721d6cc357839e0f8c9efa2508162
Leonardo Murça <leonardoamurca@gmail.com>
2020-12-16 23:44:21 -0300

    Use real database conection

--- backend/src/resolvers.js
+++ backend/src/resolvers.js
@@ -1,15 +1,12 @@
-const users = [
-    { id: 1, name: 'Leonardo', email: 'leonardo@gmail.com'},
-    { id: 2, name: 'Cristiane', email: 'cristiane@gmail.com'}
-]
+const User = require('./User')
 
 module.exports = {
     Query: {
-        users: () => users,
-        user: () => users[0]
+        users: () => User.find(),
+        user: (_, { id }) => User.findById(id)
     },
 
     Mutation: {
-        createUser: () => users[0]
+        createUser: (_, { name, email }) => User.create({ name, email })
     }
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,11 @@
 const { GraphQLServer } = require('graphql-yoga')
 const path = require('path')
+const mongoose = require('mongoose')
 const resolvers = require('./resolvers')
+
+mongoose.connect('mongodb://localhost:27017/graphqlnode', {
+    useNewUrlParser: true
+})
 
 const server = new GraphQLServer({
     typeDefs: path.resolve(__dirname, 'schema.graphql'),
